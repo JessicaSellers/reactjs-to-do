a97b852250b6dea15e4c447a1e83b3155d91d185
Jessica Sellers <jswx5@mail.missouri.edu>
2018-12-10 20:35:09 -0600

    Updates

---
+++ precache-manifest.e87a69a5580203afee415ece9bbfd8ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs-to-do/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f1925d3c35fe9b1af716",
+    "url": "/reactjs-to-do/static/js/runtime~main.f1925d3c.js"
+  },
+  {
+    "revision": "4356b4ce66e0d73c09f4",
+    "url": "/reactjs-to-do/static/js/main.4356b4ce.chunk.js"
+  },
+  {
+    "revision": "0c8d8c508ac240aeb515",
+    "url": "/reactjs-to-do/static/js/1.0c8d8c50.chunk.js"
+  },
+  {
+    "revision": "4356b4ce66e0d73c09f4",
+    "url": "/reactjs-to-do/static/css/main.caee25b1.chunk.css"
+  },
+  {
+    "revision": "798288a8cd2d9808c34d06fb406e1284",
+    "url": "/reactjs-to-do/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.43eec5c56f9e6ec939ae22cccca18011.js"
+  "/reactjs-to-do/precache-manifest.e87a69a5580203afee415ece9bbfd8ce.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/reactjs-to-do/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
